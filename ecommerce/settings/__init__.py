adab41a1db3630b47a3c01416ba2fb4b683d4119
bikerironald <bikerironald@gmail.com>
2025-07-29 02:23:18 +0300

    few changes made

--- ecommerce/settings/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-
-settings_module = os.getenv('DJANGO_SETTINGS_MODULE')
-
-if settings_module == 'ecommerce.settings.production':
-    from .production import *
-else:
-    from .development import *
